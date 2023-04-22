a97d716a2ee67705b65b21a444177a4975839e60
rencherjosh <jrencher@byu.edu>
2023-04-21 23:00:14 -0600

    Final mission05 w/ Notes

---
+++ Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission04_jrencher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission04_jrencher.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult gradeCalculator ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult gradeCalculator (gradeCalculator model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/gradeCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission04_jrencher.Models
+{
+    public class gradeCalculator
+    {
+        [Required]
+        [Range(1, 100)]
+        public string assignments { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public string groupProjects { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public string quizzes { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public string midtermExam { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public string finalExam { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public string intex { get; set; }
+        public string total { get; set; }
+        public string letterGrade { get; set; }
+
+    }
+}
